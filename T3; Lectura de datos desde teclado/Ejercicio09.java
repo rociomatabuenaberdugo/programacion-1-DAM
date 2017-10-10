fe40cb13f5f2fb4cedd6cac53ef8ac90e86ed45a
Rocío Matabuena Berdugo <holaadios2411@gmail.com>
2017-10-10 13:46:58 +0200

    Actualizado

--- T3; Lectura de datos desde teclado/Ejercicio09.java
+++ T3; Lectura de datos desde teclado/Ejercicio09.java
@@ -1,6 +1,6 @@
 /**
  *Ejercicio 9, lectura de datos desde el teclado: Escribe un programa
- * que calcule el volumen de un cono según la fórmula V=1/2πr2h.
+ * que calcule el volumen de un cono según la fórmula V=1/3πr2h.
  *
  *Rocío Matabuena Berdugo
  */
@@ -8,16 +8,16 @@
 public class Ejercicio09 {
   public static void main(String[] args) {
     
-   System.out.print("Dime el radio de tu cono: ");
+   System.out.print("Dime el radio de tu cono (en cm): ");
    double r = Double.parseDouble(System.console().readLine());
 
-   System.out.print("Dime la altura de tu cono: ");
+   System.out.print("Dime la altura de tu cono (en cm): ");
 
    double h = Double.parseDouble(System.console().readLine());
    
-   double v = (0.5 * 3.14) * (r * r) * h;
+   double v = (3.14 * r * r * h) / 3;
    
-   System.out.println("El volumen de su cono es " + v);
+   System.out.println("El volumen de su cono es " + v + " cm².");
   
   }
 }
