47a915d76c0a641ad88ea83efa7521367c0c89e6
Rocío Matabuena Berdugo <holaadios2411@gmail.com>
2017-10-16 10:58:10 +0200

    Actualizado

--- T4; Sentencia condicional/Ejercicio12.java
+++ T4; Sentencia condicional/Ejercicio12.java
@@ -1,7 +1,7 @@
 /**
  *Ejercicio 12, sentencia condicional: Realiza un minicuestionario con
  * 10 preguntas tipo test sobre las asignaturas que se imparten en el 
- * curso. CAda pregunta acertada sumará un punto. El programa mostrará
+ * curso. Cada pregunta acertada sumará un punto. El programa mostrará
  * al final la calificación obtenida. Pásale el minicuestionario a tus
  * compañeros y pídeles que lo hagan para ver qué tal andan de
  * de conocimientos en las diferentes asignaturas del curso.
