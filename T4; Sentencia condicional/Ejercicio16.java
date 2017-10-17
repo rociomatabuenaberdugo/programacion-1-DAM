168d5ab0e28054a33485b1f39a288cffb6a9f66c
Rocío Matabuena Berdugo <holaadios2411@gmail.com>
2017-10-17 13:55:19 +0200

    Actualizado

---
+++ T4; Sentencia condicional/Ejercicio16.java
@@ -0,0 +1,23 @@
+/**
+ *Ejercicio 16, sentencia condicional: Realiza un programa que nos diga
+ * si hay probabilidad de que nuestra pareja nos está siendo infiel. El
+ * programa irá haciendo preguntas que el usuario contestará con
+ * verdadero o falso. Cada pregunta contestada como verdadero sumará
+ * 3 puntos. Las preguntas contestadas con falso no suman puntos.
+ * Utiliza el fichero test_infidelidad.txt para obtener las preguntas
+ * y las conclusiones del programa.
+ *
+ *Rocío Matabuena Berdugo
+ */
+ 
+public class Ejercicio16 {
+  public static void main(String[] args) {
+   
+   System.out.println("      TEST DE INFIDELIDAD. ");
+   System.out.println("                              ");  
+   System.out.println("Este test indicará las posibilidades de que " +
+   "tu pareja te esté siendo infiel. Responde con " +
+   "(v)erdadero o (f)also. ");  
+    
+}
+}
