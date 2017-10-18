088c7f7cd35f0d88e1a76ea4b47962671a6d029b
Rocío Matabuena Berdugo <holaadios2411@gmail.com>
2017-10-18 09:15:22 +0200

    Actualizado

---
+++ T5; Bucles/Ejercicio11.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ *Ejercicio 11, bucles: Escribe un programa que muestre en tres
+ * columnas, el cuadrado y el cubo de los 5 primeros números enteros a 
+ * partir de uno que se introduce por teclado.
+ *
+ * 
+ * Rocío Matabuena Berdugo
+ * 
+ */
+public class Ejercicio11 {
+  public static void main(String[] args) {
+         System.out.println("Dime un número y te diré su cuadrado y " +
+         "su cubo, al igual que de sus siguientes cuatro números.");
+         
+         int numIntro = Integer.parseInt(System.console().readLine());
+        
+        
+    for (int i = numIntro ; i <= (numIntro+4); i++) {
+		
+		System.out.println("╭──────────┬─────────────────┬────────────────╮");
+		System.out.println("│  Número  │   Su cuadrado   │     Su cubo    │");
+		System.out.println("│──────────┴─────────────────┴────────────────│");
+		System.out.println("│   " + i + "             " + (i*i) + 
+		"               " + (i*i*i) + "        │");
+		System.out.println("╰─────────────────────────────────────────────╯");
+		
+    }
+  }
+}
