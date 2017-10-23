0b3998b5e4228beedca4a1f9d0b2687fdde72864
Rocío Matabuena Berdugo <holaadios2411@gmail.com>
2017-10-23 08:49:53 +0200

    Actualizado

---
+++ T5; Bucles/Ejercicio09.java
@@ -0,0 +1,29 @@
+/**
+ *Ejercicio 9, bucles: Realiza un programa que nos diga cuántos dígitos
+ * tiene un número introducido por teclado.
+ *
+ *Rocío Matabuena Berdugo
+ */
+
+public class Ejercicio09 {
+  public static void main(String[] args) {
+        
+    int digitos = 1, n, num;
+        
+    System.out.println("Dime un número y te diré cuántos dígitos tiene.");
+    num = Integer.parseInt(System.console().readLine());
+    
+    n = num;
+    
+    while ( n > 10) {
+		n/=10;
+		digitos++;
+	}
+	
+	System.out.println("El número " + num + " tiene " + digitos +
+	" dígito/s.");
+    
+    
+    
+    }
+  }
