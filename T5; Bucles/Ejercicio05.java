07b82fd4d5efa6476c9cbfd20138a6a93007a79a
Rocío Matabuena Berdugo <holaadios2411@gmail.com>
2017-10-23 08:25:53 +0200

    Actualizado

---
+++ T5; Bucles/Ejercicio05.java
@@ -0,0 +1,18 @@
+/**
+ *Ejercicio 5, bucles: Muestra los números del 320 al 160, contando de
+ * 20 en 20 hacia atrás utilizando un bucle while.
+ *
+ *Rocío Matabuena Berdugo
+ */
+
+public class Ejercicio05 {
+  public static void main(String[] args) {
+        
+    int i = 320;
+    
+    while (i > 159) {
+      System.out.println(i);
+      i-=20;
+    }
+  }
+}
