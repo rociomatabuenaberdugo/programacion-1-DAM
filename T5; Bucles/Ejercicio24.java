3a57a8b995624eed93d9b5ebc1d828b4eac7e42d
Rocío Matabuena Berdugo <holaadios2411@gmail.com>
2017-10-25 09:55:23 +0200

    Actualizado.

--- T5; Bucles/Ejercicio24.java
+++ T5; Bucles/Ejercicio24.java
@@ -14,6 +14,32 @@
 public class Ejercicio24 {
   public static void main(String[] args) {
  
+	int h;
+	int altura = 1;
+	int i = 0;
+	
+	System.out.println("¿Cuántas filas quieres que tenga tu " + 
+	"pirámide?");
+	h = Integer.parseInt(System.console().readLine());
  
+	int espacio = h - 1;
+	
+	while ( altura <= h ) {
+					//espacios
+		for (i=1; i <= espacio ; i++) {
+			System.out.print(" ");
+		}
+					//caracteres
+		for (i=1; i < altura; i++) {
+			System.out.print(i);
+		}
+		for ( i=altura; i > 0; i--) {
+			System.out.print(i);
+			}
+		System.out.println();
+		
+		altura++;
+		espacio--;
  }
+}
 }
