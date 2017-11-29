3376d287fc623d76ec6e81f33defdd7f81f11e8d
rociomatabuenaberdugo <holaadios2411@gmail.com>
2017-11-29 11:15:37 +0100

    Actualizado

--- T7; Arrays/Ejercicio11.java
+++ T7; Arrays/Ejercicio11.java
@@ -10,9 +10,13 @@
 * Rocío Matabuena Berdugo
 */
 
+import java.util.Scanner;
+
 public class Ejercicio11 {
   public static void main(String[] args) {
-	  
+
+		Scanner s = new Scanner(System.in);
+
 		int[] num = new int[10];
 		int[] numPrimo = new int[10];
 		int[] numNoPrimo = new int[10];
@@ -20,38 +24,64 @@ public class Ejercicio11 {
 		int j;
 		int primo = 0;
 		int noPrimo = 0;
+		boolean esPrimo = false;
 
 		System.out.println("Dime diez números enteros: ");
 
 		for (i = 0; i < 10; i++) {
 
-			num[i] = Integer.parseInt(System.console().readLine());
+			num[i] = Integer.parseInt(s.nextLine());
 
 			for (j = 2; j < num[i]; j++) {
-				if ((num[i] % j) == 0) {
-					numPrimo[primo++] = num[i];
+				if ((num[i] % j) == 1) {
+					esPrimo = true;
 				} else {
-					numNoPrimo[noPrimo++] = num[i];
+					esPrimo = false;
 				}
 			}
 
+			if (esPrimo) {
+				numPrimo[primo++] = num[i];
+			} else {
+				numNoPrimo[noPrimo++] = num[i];
+			}
 		}
 
 		System.out.println();
 		System.out.println();
+
+		System.out.println("Array original:");
+		System.out.println("*--------*-----*-----*-----*-----*-----*-----*-----*-----*-----*-----*");
+		System.out.print("* Indice ");
+		for (i = 0; i < 10; i++) {
+			System.out.printf("*%4d ", i);
+		}
+		System.out.println("*\n*--------*-----*-----*-----*-----*-----*-----*-----*-----*-----*-----*");
+		System.out.print("* Valor  ");
+		for (i = 0; i < 10; i++) {
+			System.out.printf("*%4d ", num[i]);
+		}
+		System.out.println("*\n*--------*-----*-----*-----*-----*-----*-----*-----*-----*-----*-----*");
 
 		for (i = 0; i < primo; i++) {
 			num[i] = numPrimo[i];
 		}
 
-		for (i = primo; i < 10; i++) {
-			num[i] = numNoPrimo[i - primo];
+		for (i = primo; i < primo + noPrimo; i++) {
+			num[i] = numNoPrimo[i-primo];
 		}
 
-		System.out.println("Array obtenido:");
+		System.out.println("\n\nArray con los primos al principio:");
+		System.out.println("*--------*-----*-----*-----*-----*-----*-----*-----*-----*-----*-----*");
+		System.out.print("* Indice ");
 		for (i = 0; i < 10; i++) {
-			System.out.print(num[i] + " ");
+			System.out.printf("*%4d ", i);
 		}
-
+		System.out.println("*\n*--------*-----*-----*-----*-----*-----*-----*-----*-----*-----*-----*");
+		System.out.print("* Valor  ");
+		for (i = 0; i < 10; i++) {
+			System.out.printf("*%4d ", num[i]);
+		}
+		System.out.println("*\n*--------*-----*-----*-----*-----*-----*-----*-----*-----*-----*-----*");
 	}
 }
