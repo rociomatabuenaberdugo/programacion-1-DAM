314d12c45c37849e290eaebf9ca10a9c5e8a91a2
rociomatabuenaberdugo <holaadios2411@gmail.com>
2017-12-11 10:00:12 +0100

    Actualizado

--- T7; Arrays/(2) Bidimensionales/Ejercicio05.java
+++ T7; Arrays/(2) Bidimensionales/Ejercicio05.java
@@ -11,53 +11,64 @@
 
 public class Ejercicio05 {
   public static void main(String[] args) 
-  throws InterruptedException{
+  {
 	  
-		int[][] num = new int[4][5];
+		int[][] num = new int[6][10];
+		int numMax = -1;
+		int numMin = 1001;
+		int posicionimin = 0;
+		int posicionjmin = 0;
+		int posicionimax = 0;
+		int posicionjmax = 0;
 		int fil;
 		int col;
-		int sumFil;
-		int sumCol;
 
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 5; j++) {
-				num[i][j] = (int)(Math.random()*900 + 100);
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 10; j++) {
+				num[i][j] = (int) (Math.random() * 1001);
+
+				if (numMin > num[i][j]) {
+					numMin = num[i][j];
+					posicionimin = i;
+					posicionjmin = j;
+				}
+
+				if (numMax < num[i][j]) {
+					numMax = num[i][j];
+					posicionimax = i;
+					posicionjmax = j;
+				}
+
 			}
+		}
+		
+		System.out.print("               ");
+		for (col = 0; col < 10; col++) {
+			System.out.printf("%-7s %2d   ","Columna", col + 1);
+		}
+		
+		System.out.println();
+		
+		for (fil = 0; fil < 6; fil++) {
+			System.out.print("Fila " + (fil+1) + "      ");
+			for (col = 0; col < 10; col++) {
+				System.out.printf("%10d   ", num[fil][col]);
+			}
+			System.out.println();
 		}
 
 		System.out.println();
+		System.out.println();
 
-		for (fil = 0; fil < 4; fil++) {
-			sumFil = 0;
-			for (col = 0; col < 5; col++) {
-				Thread.sleep(500);
-				System.out.printf("%7d   ", num[fil][col]);
-				sumFil += num[fil][col];
-			}
-			System.out.print("*");
-			Thread.sleep(1500);
-			System.out.printf("%7d\n", sumFil);
-		}
+		System.out.print("El número máximo de la anterior serie de"
+				+ " números se encuentra en la fila " +
+				(posicionimax+1) + " y  en la columna " +
+				(posicionjmax+1));
 
-		for (col = 0; col < 5; col++) {
-			System.out.print("***********");
-		}
-		System.out.println("***********");
+		System.out.println();
 
-		int sumTo = 0;
-		for (col = 0; col < 5; col++) {
-			sumCol = 0;
-			for (fil = 0; fil < 4; fil++) {
-				sumCol += num[fil][col];
-			}
-			
-			Thread.sleep(1500);
-			sumTo += sumCol;
-			System.out.printf("%7d   ", sumCol);
-		}
-		
-		System.out.print("*");
-		Thread.sleep(1500);
-		System.out.printf("%7d   ", sumTo);
+		System.out.print("El número mínimo de la anterior serie de"
+				+ " números se encuentra en la fila " + (posicionimin+1)
+				+ " y  en la columna " + (posicionjmin+1));
 	}
 }
