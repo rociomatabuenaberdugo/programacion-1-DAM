72c75f53dc35f68fcd9f22e20e81daff033b095b
rociomatabuenaberdugo <holaadios2411@gmail.com>
2018-02-21 09:20:17 +0100

    Actualizado

---
+++ T1; Salida de datos por pantalla/Ejercicio1.java
@@ -0,0 +1,11 @@
+/**
+ * Escribe una programa que muestre tu nombre por pantalla.
+ *
+ * @author Roframa Matber
+ */
+public class Ejercicio1 {
+  public static void main(String[] args) {
+    System.out.println("Mi nombre es Rocío Matabuena");
+  }
+  
+}
---
+++ T1; Salida de datos por pantalla/Ejercicio2.java
@@ -0,0 +1,14 @@
+/**
+ * Modifica el programa anterior para que además se muestre tu dirección y tu número de teléfono.
+ * Asegúrate de que los datos se muestran en líneas separadas.
+ *
+ * @author Roframa Matber
+ */
+public class Ejercicio2 {
+  public static void main(String[] args) {
+    System.out.println("Mi nombre es Rocío Matabuena");
+    System.out.println("Mi dirección es: Avenida Antonio Arroyo Blanco n7");
+    System.out.println("Mi número de teléfono es 638-572-069");
+  }
+  
+}
